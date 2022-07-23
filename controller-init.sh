1a156e099498de614cc0cd8c1dfab26b71ec0080
Salim Haniff <salimwp@gmail.com>
2022-07-23 10:22:32 -0400

    corrected url and changed k8s version

--- controller-init.sh
+++ controller-init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # refer to https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/create-cluster-kubeadm/
-sudo kubeadm init --pod-network-cidr 10.40.0.0/16 --kubernetes-version 1.23.0
+sudo kubeadm init --pod-network-cidr 10.40.0.0/16 --kubernetes-version 1.24.0
 
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
